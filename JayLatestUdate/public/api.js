ba44473983330a647f542582e1459eb0eaafbfc5
Macauleyt <32359167+Macauleyt@users.noreply.github.com>
2018-12-12 09:34:57 -0800

    Visualisation

---
+++ JayLatestUdate/public/api.js
@@ -0,0 +1,40 @@
+const app = document.getElementById("root");
+
+const container = document.createElement("div");
+container.setAttribute("class", "container");
+
+app.appendChild(container);
+
+var request = new XMLHttpRequest();
+request.open(
+  "GET",
+  "https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22",
+  true
+);
+request.onload = function() {
+  // Begin accessing JSON data here
+  var data = JSON.parse(this.response);
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
+      const card = document.createElement("div");
+      card.setAttribute("class", "card");
+
+      const h1 = document.createElement("h1");
+      h1.textContent = movie.title;
+
+      const p = document.createElement("p");
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
+
+      container.appendChild(card);
+      card.appendChild(h1);
+      card.appendChild(p);
+    });
+  } else {
+    const errorMessage = document.createElement("marquee");
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
+};
+
+request.send();
