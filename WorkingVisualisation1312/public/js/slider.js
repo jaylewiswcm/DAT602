dc2819c1307ed87d6d1a3ab09be274fb24584502
Jay Lewis <jr_lewis@outlook.com>
2019-01-08 10:06:39 +0000

    Intergrated Weather and News Api

--- WorkingVisualisation1312/public/js/slider.js
+++ WorkingVisualisation1312/public/js/slider.js
@@ -15,5 +15,5 @@ function showSlides() {
 
   slides[slideIndex - 1].style.display = "block";
 
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+  setTimeout(showSlides, 10000); // Change image every 5 seconds
 }
--- WorkingVisualisation1312/public/js/weather.js
+++ WorkingVisualisation1312/public/js/weather.js
@@ -1,16 +1,23 @@
-let url =
+let url2 =
   "http://api.openweathermap.org/data/2.5/weather?q=Plymouth,uk?id=524901&APPID=408a89a5a62965fbf812dcfbd64c953e";
+
+var req2 = new Request(url2);
+fetch(req2).then(function(response) {
+  console.log(response.json());
+});
 
 function getWeatherData() {
   $.ajax({
-    url: url,
+    url: url2,
     type: "GET",
     dataType: "json",
     success: function(data) {
       var temperature = data.main.temp;
+      var location = data.name;
       celsius = Math.round(temperature - 273.15);
 
-      $(".temp").html(celsius + "&#8451;");
+      $(".location-temp").html(location + ", ");
+      $(".weather-temp").html(celsius + "&#8451;");
       console.log(celsius);
     },
     error: function(err) {
